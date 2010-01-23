cdea59cd42b959a63c45789141708d038f85412a
Ulf Hermann <ulf_hermann@gmx.net>
2010-01-23 15:31:22 +0100

    move cleanup of linkstats on station deletion into capacities branch

--- src/station.cpp
+++ src/station.cpp
@@ -70,6 +70,14 @@ Station::~Station()
 		if (a->targetairport == this->index) a->targetairport = INVALID_STATION;
 	}
 
+	Station * st;
+	FOR_ALL_STATIONS(st) {
+		for (CargoID c = CT_BEGIN; c != CT_END; ++c) {
+			GoodsEntry & ge = st->goods[c];
+			ge.link_stats.erase(this->index);
+		}
+	}
+
 	Vehicle *v;
 	FOR_ALL_VEHICLES(v) {
 		/* Forget about this station if this station is removed */
