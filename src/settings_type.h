88d9f094d548d9223eaad931ca6269b3179ccc20
Ulf Hermann <ulf_hermann@gmx.net>
2010-01-23 11:48:09 +0100

    make moving average length the minimum

--- src/settings_type.h
+++ src/settings_type.h
@@ -344,7 +344,7 @@ struct EconomySettings {
 	bool   station_noise_level;              ///< build new airports when the town noise level is still within accepted limits
 	uint16 town_noise_population[3];         ///< population to base decision on noise evaluation (@see town_council_tolerance)
 	uint16 moving_average_unit;              ///< unit of time to use for calculating the moving averages
-	uint16 moving_average_length;            ///< default length of moving averages
+	uint16 moving_average_length;            ///< minimum length of moving averages
 };
 
 /** Settings related to stations. */
