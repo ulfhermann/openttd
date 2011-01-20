fb9adcad857f968e4ff52a27f1468ac94879578f
Ulf Hermann <ulf_hermann@gmx.net>
2011-01-20 23:25:53 +0100

    coding style

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1838,12 +1838,12 @@ void Vehicle::BeginLoading()
 }
 
 /**
- * return all reserved cargo packets to the station
+ * Return all reserved cargo packets to the station.
  * @param st the station where the reserved packets should go.
  */
 void Vehicle::CancelReservation(Station *st)
 {
-	for(Vehicle *v = this; v != NULL; v = v->next) {
+	for (Vehicle *v = this; v != NULL; v = v->next) {
 		VehicleCargoList &cargo = v->cargo;
 		if (cargo.ReservedCount() > 0) {
 			DEBUG(misc, 1, "cancelling cargo reservation");
