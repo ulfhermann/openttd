cd643d00356cfcd934544de236248b84fb17b68c
Ulf Hermann <ulf_hermann@gmx.net>
2011-01-20 22:46:13 +0100

    coding style

--- src/linkgraph/flowmapper.cpp
+++ src/linkgraph/flowmapper.cpp
@@ -31,9 +31,9 @@ void FlowMapper::Run(LinkGraphComponent *component)
 			assert(prev != via);
 			StationID origin = component->GetNode(path->GetOrigin()).station;
 			assert(via != origin);
-			/* mark all of the flow for local consumation at "first" */
+			/* mark all of the flow for local consumption at "first" */
 			node.flows[origin][via] += flow;
-			/* pass some of the flow marked for local consumation at "prev" on
+			/* pass some of the flow marked for local consumption at "prev" on
 			 * to this node
 			 */
 			prev_node.flows[origin][via] += flow;
@@ -47,7 +47,7 @@ void FlowMapper::Run(LinkGraphComponent *component)
 	for (NodeID node_id = 0; node_id < component->GetSize(); ++node_id) {
 		PathSet &paths = component->GetNode(node_id).paths;
 		for (PathSet::iterator i = paths.begin(); i != paths.end(); ++i) {
-			delete (*i);
+			delete *i;
 		}
 		paths.clear();
 	}
