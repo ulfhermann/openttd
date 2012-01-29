b7c70345ae7fbadf4b4369193b0cb0f3fe6d2c89
Ulf Hermann <ulf_hermann@gmx.net>
2012-01-29 16:14:09 +0100

    -Codechange: adapt normalizer to new data structure

--- src/linkgraph/normalize.cpp
+++ src/linkgraph/normalize.cpp
@@ -8,7 +8,7 @@ Normalizer::Normalizer(LinkGraphComponent *graph)
 	for (NodeID node_id = 0; node_id < graph->GetSize(); ++node_id) {
 		Node &node = graph->GetNode(node_id);
 		if (node.import_node != INVALID_NODE) {
-			if (node.export_node != node.import_node) {
+			if (node.passby != IS_PASSBY_NODE) {
 				/* Regular import node:
 				 * 1. Make an additional edge from base node to
 				 * import node.
@@ -42,34 +42,28 @@ Normalizer::Normalizer(LinkGraphComponent *graph)
 				 */
 				Node &base_node = graph->GetNode(node.export_node);
 				NodeID export_id = base_node.export_node == INVALID_NODE ?
-						node.export_node : base_node.export_node;
+						base_node : base_node.export_node;
 				NodeID remote_id = INVALID_NODE;
 				for (NodeID other_id = 0; other_id != graph->GetSize(); ++other_id) {
 					Node &other = graph->GetNode(other_id);
-					if (other.station == node.station) {
-						if (other.import_node == INVALID_STATION || other.export_node != other.import_node) {
-							/* final end of passby chain; only use if no further passby is found */
+					if (other_id == node.passby_via) {
+						if (other.passby_flag != IS_PASSBY_NODE) {
+							/* final end of passby chain */
 							remote_id = other.import_node == INVALID_STATION ? other_id : other.import_node;
 						} else {
+							remote_id = other_id;
 							/* further passby node of same chain */
-							Edge &passby_edge = graph->GetEdge(export_id, other_id);
-							if (passby_edge.capacity > 0) {
-								/* next node in passby chain */
-								uint reroute = min(node.supply, passby_edge.capacity);
-								node.supply -= reroute; // supply being misused as capacity
-								graph->GetEdge(node_id, other_id).capacity += reroute;
-								passby_edge.capacity -= reroute;
-								if (node.supply == 0) break; // no more capacity to be distributed
-							}
+							if (passby_edge.capacity == 0) continue;
 						}
+						Edge &passby_edge = graph->GetEdge(export_id, remote_id);
+						/* next node in passby chain */
+						uint reroute = min(node.supply, passby_edge.capacity);
+						node.supply -= reroute; // supply being misused as capacity
+						graph->GetEdge(node_id, remote_id).capacity += reroute;
+						passby_edge.capacity -= reroute;
+						node.passby_via = remote_id;
+						if (node.supply == 0) break; // no more capacity to be distributed
 					}
-				}
-				if (remote_id != INVALID_NODE && node.supply > 0) {
-					/* handle end of passby chain */
-					Edge &export_edge = graph->GetEdge(export_id, remote_id);
-					uint reroute = min(node.supply, export_edge.capacity);
-					export_edge.capacity -= reroute;
-					graph->GetEdge(node_id, remote_id).capacity += reroute;
 				}
 				node.supply = 0;
 				node.undelivered_supply = 0;
@@ -77,7 +71,7 @@ Normalizer::Normalizer(LinkGraphComponent *graph)
 				graph->AddEdge(export_id, node_id, UINT_MAX);
 			}
 		}
-		if (node.export_node != INVALID_NODE && node.export_node != node.import_node) {
+		if (node.export_node != INVALID_NODE && node.passby_flag != IS_PASSBY_NODE) {
 			/* Regular export node:
 			 * 1. Move all outgoing links from base node to export
 			 * node.
@@ -100,14 +94,6 @@ Normalizer::Normalizer(LinkGraphComponent *graph)
 			export_node.undelivered_supply = 0;
 			export_node.import_node = node.import_node;
 			export_node.export_node = node.export_node;
-		}
-	}
-
-	/* set passby nodes' stations to the same as base nodes' stations */
-	for (NodeID node_id = 0; node_id < graph->GetSize(); ++node_id) {
-		Node &node = graph->GetNode(node_id);
-		if (node.import_node == node.export_node && node.import_node != INVALID_NODE) {
-			node.station = graph->GetNode(node.import_node).station;
 		}
 	}
 }
