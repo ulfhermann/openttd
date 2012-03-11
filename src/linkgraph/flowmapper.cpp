e95b39686f5cdc4ad87bacf394b913fa42bc0487
Ulf Hermann <ulf_hermann@gmx.net>
2012-03-11 14:01:26 +0100

    -Fix: revert useless changes in whitespace

--- src/linkgraph/flowmapper.cpp
+++ src/linkgraph/flowmapper.cpp
@@ -20,7 +20,6 @@ void FlowMapper::Run(LinkGraphComponent *component)
 {
 	for (NodeID node_id = 0; node_id < component->GetSize(); ++node_id) {
 		Node &prev_node = component->GetNode(node_id);
-
 		StationID prev = prev_node.station;
 		PathSet &paths = prev_node.paths;
 		for (PathSet::iterator i = paths.begin(); i != paths.end(); ++i) {
@@ -31,7 +30,6 @@ void FlowMapper::Run(LinkGraphComponent *component)
 			StationID via = node.station;
 			StationID origin = component->GetNode(path->GetOrigin()).station;
 			assert(via != origin);
-
 			/* mark all of the flow for local consumption at "first" */
 			node.flows[origin][via] += flow;
 
