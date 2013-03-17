dc6d0c3d55d8e2f4918699dfe1af14d9dcfab721
Ulf Hermann <ulf_hermann@gmx.net>
2013-03-17 10:26:24 +0100

    -Add: settings for link graph

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -248,7 +248,7 @@
  *  181   25012
  *  182   25259
  */
-extern const uint16 SAVEGAME_VERSION = SL_TRUNK; ///< Current savegame version of OpenTTD.
+extern const uint16 SAVEGAME_VERSION = SL_LINKGRAPH; ///< Current savegame version of OpenTTD.
 
 SavegameType _savegame_type; ///< type of savegame we are loading
 
